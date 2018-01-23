415d962b177ea87844813620e9fffe6a3358a272
emily <yuuamelie@gmail.com>
2018-01-22 17:06:04 -0800

    mvc

--- Star Wars Encyclopedia/FilmsViewController.swift
+++ Star Wars Encyclopedia/FilmsViewController.swift
@@ -12,9 +12,7 @@ class FilmsViewController: UITableViewController {
     var films = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://swapi.co/api/films")
-        let session = URLSession.shared
-        let task = session.dataTask(with: url!, completionHandler: {
+        StarWarsModel.getAllFilms(completionHandler: {
             data, response, error in
             do {
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
@@ -22,18 +20,17 @@ class FilmsViewController: UITableViewController {
                         let resultsArray = results as! NSArray
                         for i in resultsArray{
                             let filmsDict = i as! NSDictionary
-                            self.films.append(filmsDict["title"]! as! String)
-                        }
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
+                            self.films.append(filmsDict["title"] as! String)
                         }
                     }
                 }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             } catch {
-                print(error)
+                print("error!")
             }
         })
-        task.resume()
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- Star Wars Encyclopedia/PeopleViewController.swift
+++ Star Wars Encyclopedia/PeopleViewController.swift
@@ -8,46 +8,29 @@
 
 import UIKit
 class PeopleViewController: UITableViewController {
-    // Hardcoded data for now
     var people = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // specify the url that we will be sending the GET request to
-        let url = URL(string: "https://swapi.co/api/people")
-        // create a URLSession to handle the request tasks
-        let session = URLSession.shared
-        // create a "data task" to make the request and run completion handler
-        let task = session.dataTask(with: url!, completionHandler: {
-            // see: Swift closure expression syntax
+        StarWarsModel.getAllPeople(completionHandler: {
             data, response, error in
-            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
-            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
             do {
-                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
                     if let results = jsonResult["results"]{
                         let resultsArray = results as! NSArray
-                        // now we can run NSArray methods like count and firstObject
-//                        print(resultsArray.count)
-//                        print(resultsArray[0])
-//                        print(resultsArray.firstObject)
-//                        print(jsonResult)
+
                         for i in resultsArray{
                             let people = i as! NSDictionary
                             self.people.append(people["name"] as! String)
                         }
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                        }
                     }
                 }
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                }
             } catch {
-                print(error)
+                print("error!")
             }
         })
-        // execute the task and then wait for the response
-        // to run the completion handler. This is async!
-        task.resume()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,19 +42,14 @@ class PeopleViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // if we return - sections we won't have any sections to put our rows in
         return 1
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return the count of people in our data array
         return people.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create a generic cell
         let cell = UITableViewCell()
-        // set the default cell label to the corresponding element in the people array
         cell.textLabel?.text = people[indexPath.row]
-        // return the cell so that it can be rendered
         return cell
     }
 }
